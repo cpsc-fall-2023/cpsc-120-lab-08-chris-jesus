53ce9402d637546185d580c1ab27541b634c6dd3
ChrisLCSU <143357615+ChrisLCSU@users.noreply.github.com>
2023-11-01 19:59:41 +0000

    Fixing formatting issues

--- part-2/average.cc
+++ part-2/average.cc
@@ -12,17 +12,18 @@ int main(int argc, char* argv[]) {
   if (arguments.size() <= 1) {
     std::cout << " error: you must supply at least one number\n";
     return 1;
- }
+  }
   bool flag = true;
   double sum{0.0};
-  for(std::string& argument : arguments) {
+  for (std::string& argument : arguments) {
     if (flag == true) {
       flag = false;
+
     } else {
       sum = sum + std::stod(argument);
     }
   }
   double average{sum / (static_cast<int>(arguments.size()) - 1)};
-  std::cout << "average = " << average << "\n";
+  std::cout << " average = " << average << " \n ";
   return 0;
 }
